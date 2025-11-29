f9c4cfb2fa5e2c2070fa6e4f43d9dba91f6a8b18
Neomaster X <neomasterx@outlook.com>
2025-11-29 14:39:33 +0300

    #61 ShowCommands()

--- Neomaster.JsonToLinq.Demo/Support/Menu.cs
+++ Neomaster.JsonToLinq.Demo/Support/Menu.cs
@@ -6,4 +6,20 @@ internal class Menu
   {
     Console.OutputEncoding = System.Text.Encoding.UTF8;
   }
+
+  private static void ShowCommands()
+  {
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("🔗 JsonToLinq Demos 🔗");
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine(
+      """
+      Esc   - Exit
+      1     - 
+      Other - Clear
+
+      """);
+    Console.ResetColor();
+  }
 }
