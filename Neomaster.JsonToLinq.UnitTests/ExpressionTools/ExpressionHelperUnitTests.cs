fb2e5147234109964a605abbb7559ad05216825c
Neomaster X <neomasterx@outlook.com>
2025-12-02 14:06:56 +0300

    #6 tests: ParseExpression(): single rule

--- Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionHelperUnitTests.cs
+++ Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionHelperUnitTests.cs
@@ -1,14 +1,36 @@
 using System.Linq.Expressions;
 using System.Text.Json;
+using Xunit.Abstractions;
 using static Neomaster.JsonToLinq.Consts;
 
 namespace Neomaster.JsonToLinq.UnitTests;
 
-public class ExpressionHelperUnitTests
+public class ExpressionHelperUnitTests(ITestOutputHelper output)
 {
   [Fact]
-  public void ParseExpression()
+  public void ParseExpression_SingleRule()
   {
+    var tString = typeof(string);
+    var obj = new PropertiesPublicGetSet();
+
+    foreach (var prop in obj.GetType().GetProperties())
+    {
+      var value = prop.GetValue(obj);
+      var valueString = value ?? "null";
+      if (value != null && prop.PropertyType == tString)
+      {
+        valueString = $"\"{valueString}\"";
+      }
+
+      var expectedView = $"(x.{prop.Name} == {valueString})";
+      var jsonField = Options.Default.ConvertPropertyNameForJson(prop.Name);
+      var jsonValue = JsonSerializer.Serialize(value);
+      var conditionJson = CreateConditionJson("&&", "=", jsonField, jsonValue);
+
+      ParseExpressionTest<PropertiesPublicGetSet>(conditionJson, expectedView);
+
+      output.WriteLine(expectedView);
+    }
   }
 
   [Theory]
@@ -213,7 +235,7 @@ public class ExpressionHelperUnitTests
     Assert.Equal(expectedLambdaResult, lambda.DynamicInvoke());
   }
 
-  private static void ParseExpressionTest<TItem>(string conditionJson)
+  private static void ParseExpressionTest<TItem>(string conditionJson, string expectedView)
   {
     var condition = JsonDocument.Parse(conditionJson).RootElement;
 
@@ -223,14 +245,17 @@ public class ExpressionHelperUnitTests
 
     var expr = ExpressionHelper.ParseExpression<TItem>(
       condition,
-      Expression.Parameter(typeof(TItem)),
+      Expression.Parameter(typeof(TItem), "x"),
       fieldMapper,
       Options.Default);
+
+    Assert.Equal(expectedView, expr.ToString());
   }
 
   private static string CreateConditionJson(string logic, string op, string field, string value)
   {
-    var condition = $$"""
+    var condition =
+      $$"""
       {
         "Logic": "{{logic}}",
         "Rules": [
