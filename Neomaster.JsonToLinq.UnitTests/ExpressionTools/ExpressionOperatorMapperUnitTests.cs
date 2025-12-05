e11f6c1deb9997d4d6c6f7f2d81e91014c82e7cc
Neomaster X <neomasterx@outlook.com>
2025-12-05 11:57:02 +0300

    #64 tests: ExpressionOperatorMapper ctor with other mapper source

--- Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionOperatorMapperUnitTests.cs
+++ Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionOperatorMapperUnitTests.cs
@@ -19,4 +19,21 @@ public class ExpressionOperatorMapperUnitTests
     Assert.Equal(source, mapper.Operators);
     Assert.NotStrictEqual(source, mapper.Operators);
   }
+
+  [Fact]
+  public void Ctor_Source_Mapper()
+  {
+    var source = new ExpressionOperatorMapper(
+      new Dictionary<string, ExpressionBind>
+      {
+        ["&"] = Expression.And,
+        ["|"] = Expression.Or,
+      });
+
+    var mapper = new ExpressionOperatorMapper(source);
+
+    Assert.NotEqual(source, mapper);
+    Assert.Equal(source.Operators, mapper.Operators);
+    Assert.NotStrictEqual(source.Operators, mapper.Operators);
+  }
 }
