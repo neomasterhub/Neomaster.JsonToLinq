7a950c07db129beb1a2883f1d77a7540645effb5
Neomaster X <neomasterx@outlook.com>
2025-12-06 11:57:36 +0300

    #93 Remove deprecated tests for ExpressionOperatorMapper

--- Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionOperatorMapperUnitTests.cs
+++ Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionOperatorMapperUnitTests.cs
@@ -1,42 +1,9 @@
-using System.Linq.Expressions;
 using static Neomaster.JsonToLinq.Consts;
 
 namespace Neomaster.JsonToLinq.UnitTests;
 
 public class ExpressionOperatorMapperUnitTests
 {
-  [Fact]
-  public void Ctor_Source_Dictionary()
-  {
-    var source = new Dictionary<string, ExpressionBind>
-    {
-      ["&"] = Expression.And,
-      ["|"] = Expression.Or,
-    };
-
-    var mapper = new ExpressionOperatorMapper(source);
-
-    Assert.Equal(source, mapper.Pairs);
-    Assert.NotStrictEqual(source, mapper.Pairs);
-  }
-
-  [Fact]
-  public void Ctor_Source_Mapper()
-  {
-    var source = new ExpressionOperatorMapper(
-      new Dictionary<string, ExpressionBind>
-      {
-        ["&"] = Expression.And,
-        ["|"] = Expression.Or,
-      });
-
-    var mapper = new ExpressionOperatorMapper(source);
-
-    Assert.NotEqual(source, mapper);
-    Assert.Equal(source.Pairs, mapper.Pairs);
-    Assert.NotStrictEqual(source.Pairs, mapper.Pairs);
-  }
-
   [Fact]
   public void OnDefault()
   {
@@ -59,51 +26,13 @@ public class ExpressionOperatorMapperUnitTests
   }
 
   [Fact]
-  public void Ctor_Add()
+  public void Ctor_FluentAdd_Duplicate()
   {
-    var mapper = ExpressionOperatorMapper.OnDefault();
-    var prevOperators = mapper.Clone().Pairs;
-    var newPairs = new Dictionary<string, ExpressionBind>
-    {
-      { "k1", Expression.Equal },
-      { "k2", Expression.NotEqual },
-    };
-
-    foreach (var newPair in newPairs)
-    {
-      mapper.Add(newPair.Key, newPair.Value);
-    }
-
-    Assert.All(newPairs, newPair =>
-    {
-      Assert.False(prevOperators.ContainsKey(newPair.Key));
-      Assert.True(mapper.Pairs.ContainsKey(newPair.Key));
-      Assert.Equal(newPair.Value, mapper[newPair.Key]);
-    });
-  }
-
-  [Fact]
-  public void Ctor_Add_Duplicate()
-  {
-    const string key = nameof(key);
+    const string key = "op";
     var mapper = new ExpressionOperatorMapper().Add(key, default);
     var expectedExMessage = string.Format(ErrorMessages.OperatorRegistered, key);
 
     var ex = Assert.Throws<InvalidOperationException>(() => mapper.Add(key, default));
     Assert.Equal(expectedExMessage, ex.Message);
-  }
-
-  [Fact]
-  public void TryGet()
-  {
-    var mapper = new ExpressionOperatorMapper().Add("&", Expression.And);
-
-    var andSuccess = mapper.TryGet("&", out var andExpr);
-    var orSuccess = mapper.TryGet("|", out var orExpr);
-
-    Assert.True(andSuccess);
-    Assert.Equal(Expression.And, andExpr);
-    Assert.False(orSuccess);
-    Assert.Null(orExpr);
   }
 }
