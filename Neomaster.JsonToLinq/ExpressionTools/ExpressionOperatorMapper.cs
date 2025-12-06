5365f20f26c534c46df9a8092217f8b63486b664
Neomaster X <neomasterx@outlook.com>
2025-12-06 10:56:57 +0300

    #91 ExpressionOperatorMapper : IFluentAddPair

--- Neomaster.JsonToLinq/ExpressionTools/ExpressionOperatorMapper.cs
+++ Neomaster.JsonToLinq/ExpressionTools/ExpressionOperatorMapper.cs
@@ -2,28 +2,45 @@ using static Neomaster.JsonToLinq.Consts;
 
 namespace Neomaster.JsonToLinq;
 
-public class ExpressionOperatorMapper : Mapper<string, ExpressionBind>, ICloneable<ExpressionOperatorMapper>
+public class ExpressionOperatorMapper
+  : Mapper<string, ExpressionBind>,
+  IFluentAddPair<ExpressionOperatorMapper, string, ExpressionBind>,
+  ICloneable<ExpressionOperatorMapper>
 {
   static ExpressionOperatorMapper()
   {
     Default = ExpressionOperatorMappers.Default;
   }
 
-  public override Mapper<string, ExpressionBind> Add(
-    string key,
-    ExpressionBind value,
-    string errorMessage = ErrorMessages.OperatorRegistered)
+  public ExpressionOperatorMapper()
+    : base()
   {
-    return base.Add(key, value, errorMessage);
+  }
+
+  public ExpressionOperatorMapper(ExpressionOperatorMapper source)
+    : base(source)
+  {
+  }
+
+  public ExpressionOperatorMapper(IDictionary<string, ExpressionBind> sourcePairs)
+    : base(sourcePairs)
+  {
+  }
+
+  public ExpressionOperatorMapper Add(string key, ExpressionBind value)
+  {
+    Add(key, value, ErrorMessages.OperatorRegistered);
+
+    return this;
   }
 
   public ExpressionOperatorMapper Clone()
   {
     var clone = new ExpressionOperatorMapper();
 
-    foreach (var op in _operators)
+    foreach (var p in Pairs)
     {
-      clone.Add(op.Key, op.Value);
+      clone.Add(p.Key, p.Value);
     }
 
     return clone;
