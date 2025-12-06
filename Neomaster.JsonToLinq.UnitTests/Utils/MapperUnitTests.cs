eee760b748d7db21f86066bc843cfb8259895880
Neomaster X <neomasterx@outlook.com>
2025-12-06 11:38:15 +0300

    #93 tests: Mapper: ctor with source mapper

--- Neomaster.JsonToLinq.UnitTests/Utils/MapperUnitTests.cs
+++ Neomaster.JsonToLinq.UnitTests/Utils/MapperUnitTests.cs
@@ -16,4 +16,21 @@ public class MapperUnitTests
     Assert.Equal(source, mapper.Pairs);
     Assert.NotStrictEqual(source, mapper.Pairs);
   }
+
+  [Fact]
+  public void Ctor_Source_Mapper()
+  {
+    var source = new Mapper<int, string>(
+      new Dictionary<int, string>
+      {
+        [1] = "1",
+        [2] = "2",
+      });
+
+    var mapper = new Mapper<int, string>(source);
+
+    Assert.NotEqual(source, mapper);
+    Assert.Equal(source.Pairs, mapper.Pairs);
+    Assert.NotStrictEqual(source.Pairs, mapper.Pairs);
+  }
 }
