26d74992546010a09fcf584b39e2033b6cb55f2e
Neomaster X <neomasterx@outlook.com>
2025-12-06 11:41:41 +0300

    #93 tests: Mapper: Add()

--- Neomaster.JsonToLinq.UnitTests/Utils/MapperUnitTests.cs
+++ Neomaster.JsonToLinq.UnitTests/Utils/MapperUnitTests.cs
@@ -33,4 +33,34 @@ public class MapperUnitTests
     Assert.Equal(source.Pairs, mapper.Pairs);
     Assert.NotStrictEqual(source.Pairs, mapper.Pairs);
   }
+
+  [Fact]
+  public void Ctor_Add()
+  {
+    var mapper = new Mapper<int, string>(
+      new Dictionary<int, string>
+      {
+        [1] = "1",
+        [2] = "2",
+      });
+
+    var prevPairs = mapper.Pairs.ToDictionary();
+    var newPairs = new Dictionary<int, string>
+    {
+      [3] = "3",
+      [4] = "4",
+    };
+
+    foreach (var newPair in newPairs)
+    {
+      mapper.Add(newPair.Key, newPair.Value);
+    }
+
+    Assert.All(newPairs, newPair =>
+    {
+      Assert.False(prevPairs.ContainsKey(newPair.Key));
+      Assert.True(mapper.Pairs.ContainsKey(newPair.Key));
+      Assert.Equal(newPair.Value, mapper[newPair.Key]);
+    });
+  }
 }
