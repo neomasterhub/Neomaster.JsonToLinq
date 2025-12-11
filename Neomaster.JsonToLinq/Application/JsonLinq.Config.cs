e5e2111c9b8c5cfd22704e524bda512ff2dcb389
Neomaster X <neomasterx@outlook.com>
2025-12-11 11:31:11 +0300

    #48 RestoreDefaultOptions()

--- Neomaster.JsonToLinq/Application/JsonLinq.Config.cs
+++ Neomaster.JsonToLinq/Application/JsonLinq.Config.cs
@@ -5,6 +5,11 @@ namespace Neomaster.JsonToLinq;
 
 public static partial class JsonLinq
 {
+  public static void RestoreDefaultOptions()
+  {
+    Options.Default = null;
+  }
+
   public static void Configure(Action<ExpressionParsingOptions> setOptions)
   {
     Options.Default = new();
