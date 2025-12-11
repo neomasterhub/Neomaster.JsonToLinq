e5ffddb798a22416ae7d83225add85eef579bf55
Neomaster X <neomasterx@outlook.com>
2025-12-11 11:46:58 +0300

    #48 Configure method summaries

--- Neomaster.JsonToLinq/Application/JsonLinq.Config.cs
+++ Neomaster.JsonToLinq/Application/JsonLinq.Config.cs
@@ -5,11 +5,18 @@ namespace Neomaster.JsonToLinq;
 
 public static partial class JsonLinq
 {
+  /// <summary>
+  /// Restores the default options.
+  /// </summary>
   public static void RestoreDefaultOptions()
   {
     Options.Default = null;
   }
 
+  /// <summary>
+  /// Applies custom options as the default.
+  /// </summary>
+  /// <param name="setOptions">Sets custom options as the default.</param>
   public static void Configure(Action<ExpressionParsingOptions> setOptions)
   {
     Options.Default = new();
