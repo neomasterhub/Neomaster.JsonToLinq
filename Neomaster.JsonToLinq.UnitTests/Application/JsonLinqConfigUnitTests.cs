7c3198a3216f0344f2f73d92b729da58fa78bb36
Neomaster X <neomasterx@outlook.com>
2025-12-11 11:16:25 +0300

    #48 tests: Configure()

---
+++ Neomaster.JsonToLinq.UnitTests/Application/JsonLinqConfigUnitTests.cs
@@ -0,0 +1,59 @@
+using System.Linq.Expressions;
+using System.Text.Json;
+using static Neomaster.JsonToLinq.Consts;
+
+namespace Neomaster.JsonToLinq.UnitTests;
+
+public class JsonLinqConfigUnitTests
+{
+  [Fact]
+  public void Configure_AllDifferent()
+  {
+    var prevDefaultOptions = Options.Default;
+
+    JsonLinq.Configure(options =>
+    {
+      options.LogicOperatorPropertyName = "L";
+      options.RulesPropertyName = "R";
+      options.OperatorPropertyName = "O";
+      options.FieldPropertyName = "F";
+      options.ValuePropertyName = "V";
+      options.OperatorMapper = new ExpressionOperatorMapper()
+        .Add("OR", Expression.OrElse)
+        .Add("EQ", Expression.Equal)
+        .Add("LT", Expression.LessThan);
+      options.BindBuilder = ExpressionBindBuilders.NullAsFalse;
+      options.ConvertPropertyNameForJson = JsonNamingPolicy.SnakeCaseUpper.ConvertName;
+    });
+
+    var json =
+      """
+      {
+        "L": "OR",
+        "R": [
+          {
+            "F": "LAST_VISIT_AT",
+            "O": "EQ",
+            "V": null
+          },
+          {
+            "F": "LAST_VISIT_AT",
+            "O": "LT",
+            "V": "2025-01-01T00:00:00Z"
+          }
+        ]
+      }
+      """;
+
+    var ex = Record.Exception(() => JsonLinq.ParseToFilterExpression<User>(JsonDocument.Parse(json)));
+
+    Assert.NotEqual(prevDefaultOptions, Options.Default);
+    Assert.Null(ex);
+
+    var props = typeof(ExpressionParsingOptions).GetProperties();
+    foreach (var prop in props)
+    {
+      Assert.NotEqual(prop.GetValue(prevDefaultOptions), prop.GetValue(Options.Default));
+    }
+  }
+}
