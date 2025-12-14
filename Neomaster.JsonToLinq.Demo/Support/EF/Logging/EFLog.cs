55d6a6e128743ce5853d2f3a2b59d1a620830ee0
Neomaster X <neomasterx@outlook.com>
2025-12-14 09:15:30 +0300

    #128 EFLog

---
+++ Neomaster.JsonToLinq.Demo/Support/EF/Logging/EFLog.cs
@@ -0,0 +1,56 @@
+namespace Neomaster.JsonToLinq.Demo;
+
+internal class EFLog
+{
+  private readonly List<LogEvent> _events = [];
+
+  public IReadOnlyList<LogEvent> Events => _events;
+
+  public void Add(LogEvent e)
+  {
+    _events.Add(e);
+  }
+
+  public void Print()
+  {
+    Console.ResetColor();
+
+    for (var i = 0; i < _events.Count; i++)
+    {
+      var e = _events[i];
+
+      Console.Write($"{i}. ");
+
+      switch (e.LogLevel)
+      {
+        case Microsoft.Extensions.Logging.LogLevel.Trace:
+          break;
+        case Microsoft.Extensions.Logging.LogLevel.Debug:
+          break;
+        case Microsoft.Extensions.Logging.LogLevel.Information:
+          Console.ForegroundColor = ConsoleColor.Cyan;
+          break;
+        case Microsoft.Extensions.Logging.LogLevel.Warning:
+          Console.ForegroundColor = ConsoleColor.Yellow;
+          break;
+        case Microsoft.Extensions.Logging.LogLevel.Error:
+          Console.ForegroundColor = ConsoleColor.Red;
+          break;
+        case Microsoft.Extensions.Logging.LogLevel.Critical:
+          Console.ForegroundColor = ConsoleColor.Red;
+          break;
+        case Microsoft.Extensions.Logging.LogLevel.None:
+          break;
+        default: throw new ArgumentOutOfRangeException(nameof(e.LogLevel));
+      }
+
+      Console.Write($"[{e.LogLevel}] ");
+
+      Console.ResetColor();
+
+      Console.WriteLine(e.Text);
+    }
+
+    Console.ResetColor();
+  }
+}
