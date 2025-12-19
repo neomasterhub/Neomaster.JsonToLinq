9819e2fd4138c51a91e273cc1daea59c0e0e8313
Neomaster X <neomasterx@outlook.com>
2025-12-19 14:09:54 +0300

    #147 Demo: "as upper in"

--- Neomaster.JsonToLinq.Demo/Services/UserDemoService.cs
+++ Neomaster.JsonToLinq.Demo/Services/UserDemoService.cs
@@ -382,6 +382,71 @@ internal class UserDemoService
   }
 
   /// <summary>
+  /// Operator <c>as upper in</c>.
+  /// </summary>
+  public void AsUpperIn(Log log)
+  {
+    using var dbContext = new AppDbContext();
+
+    var firstNames = dbContext.Users
+      .Take(4)
+      .Select(u => u.FirstName)
+      .AsEnumerable()
+      .Select((e, i) => i < 2
+        ? e.ToLower()
+        : e.ToUpper())
+      .ToList();
+
+    var firstNamesString = string.Join(",\n        ", firstNames.Select(e => $"\"{e}\""));
+
+    var filterJson =
+      $$"""
+      {
+        "Logic": "||",
+        "Rules": [
+          {
+            "Field": "firstName",
+            "Operator": "as upper in",
+            "Value": [
+              {{firstNamesString}}
+            ]
+          }
+        ]
+      }
+      """;
+
+    try
+    {
+      var expected = dbContext.Users
+        .Where(u => firstNames.Contains(u.FirstName.ToUpper()))
+        .ToArray();
+
+      var actual = dbContext.Users
+        .Where(JsonLinq.ParseToFilterExpression<User>(filterJson))
+        .ToArray();
+
+      Assert.True(expected.Length > 0);
+      Assert.Equal(expected.Length, actual.Length);
+      Assert.Equal(expected.Select(u => u.Id), actual.Select(u => u.Id));
+
+      log.Add($"Filter:\n{filterJson}");
+      log.AddSep();
+
+      log.Add("Found first names:");
+      foreach (var u in actual.DistinctBy(u => u.FirstName))
+      {
+        log.Add(u.FirstName);
+      }
+
+      log.Add(NoteString, textColor: ConsoleColor.DarkCyan);
+    }
+    catch (Exception ex)
+    {
+      log.Add(ex.Message, LogLevel.Error);
+    }
+  }
+
+  /// <summary>
   /// Custom operators:
   /// <list type="number">
   /// <item>
