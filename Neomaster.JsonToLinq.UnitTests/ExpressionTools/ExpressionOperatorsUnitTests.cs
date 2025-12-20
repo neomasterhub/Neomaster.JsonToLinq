4a6d5a3ce1ad8492ce84481df8062a7d5351394d
Neomaster X <neomasterx@outlook.com>
2025-12-20 13:50:43 +0300

    #14 tests: Contains_AsUpper()

--- Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionOperatorsUnitTests.cs
+++ Neomaster.JsonToLinq.UnitTests/ExpressionTools/ExpressionOperatorsUnitTests.cs
@@ -424,6 +424,28 @@ public class ExpressionOperatorsUnitTests(ITestOutputHelper output)
     actual.ForEach(u => output.WriteLine(u.FirstName));
   }
 
+  [Fact]
+  public void Contains_AsUpper()
+  {
+    const string infix = "A";
+    var users = new User[]
+    {
+      new() { FirstName = "-a-" },
+      new() { FirstName = "-A-" },
+    };
+    var expected = users.Where(u => u.FirstName.ToUpper().Contains(infix));
+    var func = CreateContainsFunc<User, string>(
+      nameof(User.FirstName),
+      infix,
+      StringTransform.Upper);
+
+    var actual = users.Where(func).ToList();
+
+    Assert.Equal(expected, actual);
+
+    actual.ForEach(u => output.WriteLine(u.FirstName));
+  }
+
   private static Func<TEntity, bool> CreateContainsFunc<TEntity, TProp>(
     string propName,
     string prefix,
